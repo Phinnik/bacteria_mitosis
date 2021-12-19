cedc40ca218ba4af4f571e797dcda7918d4bb22e
phinnik <phinnik@mail.ru>
2021-12-19 20:52:56 +0300

    initialize project structure

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bacteria mitosis detection
+
+Siamese network for mitosis detection
+
+## Data
+
+https://osf.io/gdxen/
+
+## Setup
+
+```shell
+pip install -r requirements.txt
+pip instll -e .
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bacteria_mitosis',
+    author='phinnik',
+    author_email='elecdron@gmail.com',
+    version='0.0.1',
+    packages=find_packages(),
+)

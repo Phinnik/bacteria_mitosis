dd1208d3d0e486ee4a8b72ab6e1a09231487cf01
phinnik <phinnik@mail.ru>
2021-12-20 08:36:35 +0300

    Make basic training loop class

--- bm/config.py
+++ bm/config.py
@@ -1,6 +1,8 @@
 import dataclasses
 import pathlib
 
+import torch
+
 BASE_DIR = pathlib.Path(__file__).parent.parent.absolute()
 
 
@@ -15,3 +17,5 @@ class Config:
     random_seed = 7462294
     train_size = 0.7
     val_size = 0.15
+    project_name = 'bacteria_mitosis'
+    device = torch.device('cuda')
